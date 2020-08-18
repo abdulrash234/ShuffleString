38e1e6236bb8a408d200f8afd4a0636604d669ef
Abdulrashid Mohammed <abmoh@dfds.com>
2020-08-18 14:55:23 +0200

    leetcode shuffle string problem

---
+++ ShuffleString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ShuffleString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+            Console.WriteLine(RestoreString("codeleet", indices));
+        }
+
+        public static string RestoreString(string s, int[] indices)
+        {
+
+            var arrayShuffled = new char[indices.Length];
+            var returnedString = "";
+            var i = 0;
+            foreach (var letter in s)
+            {
+                arrayShuffled[indices[i]] = letter;
+                i++;
+            }
+
+
+            foreach (var c in arrayShuffled)
+            {
+                returnedString = returnedString + c;
+            }
+
+            return returnedString;
+        }
+    }
+}
